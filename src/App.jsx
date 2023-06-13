fb9c8527894fa37940ca91bbdd1cfa5fffd5ceb7
soongonkim <soongon@hucloud.co.kr>
2023-06-13 12:57:16 +0900

    chap05-loop02

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import './App.css'
-import PackingList from './components/PackingList'
+import './App.css';
+import { people } from './data.js';
 
-export default function App() {
-  return (
-    <PackingList/>
+export default function List() {
+
+  const personList = people.map(person => 
+    <li key={person.id}>
+      <img
+        src={person.imageUrl}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
   )
+
+  return (
+    <ul>{personList}</ul>
+  );
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const people = [
+  {
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageUrl: 'https://i.imgur.com/MK3eW3A.jpg'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageUrl: 'https://i.imgur.com/mynHUSa.jpg'
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageUrl: 'https://i.imgur.com/bE7W1ji.jpg'
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageUrl: 'https://i.imgur.com/IOjWm71.jpg'
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageUrl: 'https://i.imgur.com/lrWQx8l.jpg'
+}];
