298f6a7b9addd0e58ed6f0c4843d5cd400ada000
soongonkim <soongon@hucloud.co.kr>
2023-07-09 19:44:31 +0900

    chap08

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
   const [todos, setTodos] = useState([
-    {title: '고객에게 메일보내기', completed: false},
-    {title: '저녁 미팅', completed: false},
-    {title: '집 청소하기', completed: true},
+    {
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    }
   ]);
 
-  const onTodoType = e => {
-    setText(e.target.value);
+  useEffect(() => {
+    fetchTodosFromServer();
+  }, []);
+
+  const onTodoDataFetch = async () => {
+    fetchTodosFromServer();
   };
 
-  const onAddTodo = () => {
-    // array 에 아이템을 추가하기 위해서 immutable 한 방식을 사용한다.
-    // setTodos(todos.append({title: text, completed: false})); - 작동 안됨
-    setTodos([
-      ...todos,
-      {title: text, completed: false}
-    ])
+  async function fetchTodosFromServer() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const json = await res.json();
+    setTodos(json);
   }
 
-  const increase = () => {
-    setCount(count + 1);
-  }
-  const decrease = () => {
-    setCount(count - 1);
-  }
   return (
     <>
-      <button onClick={decrease}>-</button> {count} <button onClick={increase}>+</button>
+      <p>데이터 fetching과 useEffect 훅</p>
       <hr/>
-      <h3>할일 목록</h3>
-      <input type="text" value={text} onChange={onTodoType}/ > 
-      <button onClick={onAddTodo}>할일추가</button>
+      <button onClick={onTodoDataFetch}>서버에서 데이터 가져오기</button>
+      <p>할일 목록</p>
       <ul>
         {
           todos.map(todo => <li>{todo.title} - {todo.completed ? '완료' : '진행중'}</li>)
